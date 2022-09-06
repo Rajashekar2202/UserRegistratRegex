adc024a10ba228cc629c36163676f07bc1af5e82
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 19:29:26 +0530

    Added Mobile number program

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace UserRegistration1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+
+            User a = new User();
+            bool val = a.FirstName(firstName);
+
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+
+            User b = new User();
+            bool val1 = b.LastName(lastName);
+
+            Console.WriteLine("Enter Email Id");
+            string emailId = Console.ReadLine();
+
+            User c = new User();
+            bool val2 = c.EmailId(emailId);
+
+            Console.WriteLine("Enter Mobile Number");
+            string mobileNumber = Console.ReadLine();
+
+            User d = new User();
+            bool val3 = d.MobileNumber(mobileNumber);
+
+            if (val)
+            {
+                Console.WriteLine("Valid FirstName");
+            }
+            else
+            {
+                Console.WriteLine("Invalid FirstName");
+            }
+
+            if (val1)
+            {
+                Console.WriteLine("Valid LastName");
+            }
+            else
+            {
+                Console.WriteLine("Invalid LastName");
+            }
+
+            if (val2)
+            {
+                Console.WriteLine("Valid Email");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Email");
+            }
+
+            if (val3)
+            {
+                Console.WriteLine("Valid MobileNumber");
+            }
+            else
+            {
+                Console.WriteLine("Invalid MobilNumber");
+            }
+        }
+    }
+}
