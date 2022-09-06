9829bcae316cdb33b5be0d3dfe0589e5d9120245
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 19:20:23 +0530

    Added Email Id program

---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration1
+{
+    public class User
+    {
+        public static string User_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, User_FirstName);
+        }
+
+        public static string User_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, User_LastName);
+        }
+
+        public static string User_EmailId = "^[A-Za-z]{3,}([.][A-Za-z]{3,})?[@][A-Za-z]{2,}([.][A-Za-z]{2})?$";
+        public bool EmailId(string emailId)
+        {
+            return Regex.IsMatch(emailId, User_EmailId);
+        }
+    }
+}
