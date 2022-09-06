033bf05707889ceaf2a2f634e6fa8a5c77c48a3e
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 18:48:02 +0530

    Added FirstName program

---
+++ FirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration1
+{
+    public class User
+    {
+        public static string User_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, User_FirstName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UserRegistration1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+
+            User a = new User();
+            bool val = a.FirstName(firstName);
+            if (val)
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+    }
+}
