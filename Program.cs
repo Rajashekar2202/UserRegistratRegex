c903ed505c8b5ed06d375d47f28ab14d77b26794
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-09-06 18:59:10 +0530

    Added LastName program

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace UserRegistration1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+
+            User a = new User();
+            bool val = a.FirstName(firstName);
+
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+
+            User b = new User();
+            bool val1 = b.LastName(lastName);
+            if (val)
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration1
+{
+    public class User
+    {
+        public static string User_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, User_FirstName);
+        }
+
+        public static string User_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, User_LastName);
+        }
+    }
+}
